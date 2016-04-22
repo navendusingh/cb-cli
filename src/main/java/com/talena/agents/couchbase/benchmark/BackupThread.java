a983840aaaae51cd670996e4d0e1b19e6f2ae261
navendusingh <nsingh@talena-inc.com>
2016-04-22 15:55:43 +0530

    Added backup benchmarking code
    BackupThread class allows creation of multiple threads, each can
    backup a set of vBuckets.
    
    BenchmarkBackup parses commandline an accordingly instantiate
    BackupThread instances.

---
+++ src/main/java/com/talena/agents/couchbase/benchmark/BackupThread.java
@@ -0,0 +1,178 @@
+package com.talena.agents.couchbase.benchmark;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.couchbase.client.core.message.dcp.DCPRequest;
+import com.couchbase.client.core.message.dcp.MutationMessage;
+import com.couchbase.client.core.message.dcp.RemoveMessage;
+import com.talena.agents.couchbase.core.ClosedPartition;
+import com.talena.agents.couchbase.core.CouchbaseFacade;
+import com.talena.agents.couchbase.core.DCPEndpoint;
+import com.talena.agents.couchbase.core.OpenPartition;
+import com.talena.agents.couchbase.core.PartitionSpec;
+import com.talena.agents.couchbase.core.PartitionStats;
+
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.observables.MathObservable;
+
+public class BackupThread extends Thread {
+  private String bucketName;
+  private String password;
+  private String[] nodes;
+  private String backupPath;
+  private Map<Short, PartitionSpec> specs;
+
+  private long bytesCount = 0;
+  private long startTime = 0;
+  private long endTime = 0;
+  private FileChannel fcOut = null;
+  private AtomicLong mutationCount;
+
+  private long flushCounter = 0;
+  private final long flushLimit = (10L * 20L * 1024L * 1024L);
+
+  public long getMutationsReceived() {
+    return mutationCount.get();
+  }
+
+  public long getBytesReceived() {
+    return bytesCount;
+  }
+
+  public long getStartTime() {
+    return startTime;
+  }
+
+  public long getEndTime() {
+    return endTime;
+  }
+
+  public BackupThread(
+      final String threadName,
+      final String bucketName,
+      final String password,
+      final String[] nodes,
+      final String backupPath,
+      final Map<Short, PartitionSpec> specs) {
+    super(threadName);
+
+    this.bucketName = bucketName;
+    this.password = password;
+    this.nodes = nodes.clone();
+    this.backupPath = backupPath;
+    this.specs = new HashMap<Short, PartitionSpec>(specs);
+  }
+
+  public void run() {
+    System.out.println("[" + this.getName() + "] Starting.");
+
+    CouchbaseFacade cbFacade = new CouchbaseFacade(nodes, bucketName, password);
+    cbFacade.openBucket();
+
+    File backupFile = new File(backupPath + "backup.data.full");
+
+    try {
+      fcOut = new FileOutputStream(backupFile).getChannel();
+    } catch (FileNotFoundException e1) {
+      e1.printStackTrace();
+    }
+
+    DCPEndpoint dcp = cbFacade.dcpEndpoint();
+    dcp.openConnection(this.getName(), new HashMap<String, String>());
+
+    mutationCount = new AtomicLong(0);
+
+    startTime = System.currentTimeMillis();
+
+    dcp.streamPartitions(specs)
+    .toBlocking()
+    .forEach(new Action1<DCPRequest>() {
+      public void call(DCPRequest dcpRequest) {
+        handleDCPRequest(dcpRequest);
+      }
+    });
+
+    dcp.closedPartitions()
+    .toBlocking()
+    .forEach(new Action1<ClosedPartition>() {
+      public void call(ClosedPartition p) {
+      }
+    });
+
+    dcp.openPartitions()
+    .toBlocking()
+    .forEach(new Action1<OpenPartition>() {
+      public void call(OpenPartition p) {
+      }
+    });
+
+    endTime = System.currentTimeMillis();
+
+    long mCount = MathObservable.sumLong(dcp
+        .stats()
+        .map(new Func1<PartitionStats, Long>() {
+          public Long call(PartitionStats s) {
+            return s.stats().get(PartitionStats.Stat.NUM_MUTATIONS);
+          }
+        }))
+        .single()
+        .toBlocking()
+        .first();
+
+    try {
+      if (fcOut != null) {
+        fcOut.force(true);
+        fcOut.close();
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+
+    System.out.println("[" + this.getName()
+        + "] Ending. Written " + mCount + " mutation(s) to "
+        + backupPath);
+  }
+
+  private void handleDCPRequest(final DCPRequest req) {
+    if (req instanceof MutationMessage) {
+      MutationMessage msg = (MutationMessage) req;
+      handleMutation(msg, msg.partition());
+    } else if (req instanceof RemoveMessage) {
+      RemoveMessage msg = (RemoveMessage) req;
+      handleRemove(msg, msg.partition());
+    }
+  }
+
+  private void handleMutation(final MutationMessage msg, short partition) {
+    mutationCount.incrementAndGet();
+
+    try {
+      msg.content().getBytes(0, fcOut, msg.content().capacity());
+      flushCounter += msg.content().capacity();
+
+      if (flushCounter >= flushLimit) {
+        fcOut.force(true);
+        flushCounter = 0;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.print("\r[" + this.getName()
+        + "] Written mutation # " + mutationCount.get());
+
+    bytesCount += msg.content().capacity();
+    msg.content().release();
+    msg.connection().consumed(msg);
+  }
+
+  private void handleRemove(final RemoveMessage msg, short partition) {
+  }
+}
---
+++ src/main/java/com/talena/agents/couchbase/benchmark/BenchmarkBackup.java
@@ -0,0 +1,159 @@
+package com.talena.agents.couchbase.benchmark;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.talena.agents.couchbase.core.CouchbaseFacade;
+import com.talena.agents.couchbase.core.PartitionSpec;
+
+public class BenchmarkBackup {
+  private static void printUsage() {
+    System.out.println(
+        "Usage: <bucket> <backupPaths> <Full/Incremental> <ipNodes> <threads>");
+  }
+
+  public static void main(String[] args) {
+    if (args.length < 5) {
+      printUsage();
+
+      return;
+    }
+
+    String bucket = args[0];
+    String backupPaths[] = args[1].split(":");
+    String backupType = args[2];
+    String nodes[] = args[3].split(",");
+    int threadsCount = 1;
+    boolean isFullBackup = true;
+
+    try {
+      threadsCount = Integer.parseInt(args[4]);
+    } catch (NumberFormatException e) {
+      printUsage();
+
+      return;
+    }
+
+    if (backupType.compareToIgnoreCase("Full") == 0) {
+      isFullBackup = true;
+    } else if (backupType.compareToIgnoreCase("Incremental") == 0) {
+      isFullBackup = false;
+    } else {
+      printUsage();
+
+      return;
+    }
+
+    BenchmarkBackup app = new BenchmarkBackup();
+    app.Run(bucket, nodes, backupPaths, isFullBackup, threadsCount);
+
+    System.out.print("Backup Type    : ");
+    if (backupType.compareToIgnoreCase("Full") == 0) {
+      System.out.println("Full");
+    } else if (backupType.compareToIgnoreCase("Incremental") == 0) {
+      System.out.println("Incremental");
+    }
+
+    System.out.println("Bucket         : " + bucket);
+
+    System.out.println("Backup Path(s) : ");
+    for (String backupPath : backupPaths) {
+      System.out.println("\t" + backupPath);
+    }
+
+    System.out.println("Node(s)        : ");
+    for (String node : nodes) {
+      System.out.println("\t" + node);
+    }
+
+    System.out.println("Thread count   : " + threadsCount);
+  }
+
+  public void Run(String bucket, String[] nodes, String[] backupPaths,
+      boolean isFullBackup, int threadsCount) {
+    CouchbaseFacade cbFacade = new CouchbaseFacade(nodes, bucket, "");
+    cbFacade.openBucket();
+
+    Map<Short, Long> highSeqNos = cbFacade.currentHighSeqnos();
+    short changedPartitions[] = new short[1024];
+    int changedPartitionsCount = 0;
+
+    System.out.println("vBuckets high sequence numbers are ...");
+
+    for (Map.Entry<Short, Long> highSeqNo : highSeqNos.entrySet()) {
+      if (highSeqNo.getValue() != 0) {
+        System.out.println(highSeqNo.getKey() + " " + highSeqNo.getValue());
+        changedPartitions[changedPartitionsCount] = highSeqNo.getKey().shortValue();
+        ++changedPartitionsCount;
+      }
+    }
+
+    Map<Short, PartitionSpec> specs[] =
+        (Map<Short, PartitionSpec>[])new Map<?, ?>[threadsCount];
+    for (int i = 0; i < threadsCount; ++i) {
+      specs[i] = new HashMap<Short, PartitionSpec>();
+    }
+    for (int i = 0; i < changedPartitionsCount; ++i) {
+      short idChanged = changedPartitions[i];
+      long endSeqNo = highSeqNos.get(idChanged);
+      long startSeqNo = 0;
+
+      if (endSeqNo > startSeqNo) {
+        PartitionSpec partSpec = new PartitionSpec(
+            idChanged, 0, startSeqNo, endSeqNo, 0, 0);
+        System.out.println("For partition " + idChanged
+            + " range is " + startSeqNo + " to " + endSeqNo);
+  
+        int specsIndex = i % threadsCount;
+
+        specs[specsIndex].put(idChanged, partSpec);
+      }
+    }
+
+    for (int i = 3; i >= 1; --i) {
+      System.out.print("\rStarting in " + i + " seconds.");
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+      }
+    }
+    System.out.println("");
+
+    BackupThread threads[] = new BackupThread[threadsCount];
+
+    for (int i = 0; i < threadsCount; ++i) {
+      threads[i] = new BackupThread(
+          "Thread " + (i + 1),
+          bucket, "", nodes, backupPaths[i % backupPaths.length]
+          + "thread" + (i + 1) + ".", specs[i]);
+      System.out.println("Created Thread " + (i + 1));
+    }
+
+    for (int i = 0; i < threadsCount; ++i) {
+      threads[i].start();
+    }
+
+    try {
+      for (BackupThread thread : threads) {
+        thread.join();
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+    System.out.println("All mutations received.");
+
+    for (int i = 0; i < threadsCount; ++i) {
+      long startTime = threads[i].getStartTime();
+      long endTime = threads[i].getEndTime();
+      long timeTaken = endTime - startTime;
+
+      System.out.println("Thread " + (i + 1) + "[Mutations="
+          + threads[i].getMutationsReceived()
+          + ", Bytes=" + threads[i].getBytesReceived()
+          + ", Start Time=" + startTime
+          + ", End Time=" + endTime
+          + ", Time Taken=" + timeTaken + "ms]");
+    }
+  }
+}
